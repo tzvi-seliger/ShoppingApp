3433fa3c0dd1eee832296d96c5c4f0b44e61125f
unknown <tzviseliger@gmail.com>
2019-07-25 02:06:23 -0400

    done 4

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -2,17 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-value',
-  templateUrl: './value.component.html',
-  styleUrls: ['./value.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class ValueComponent implements OnInit {
-// dont need to specify type for property
-values: any;
+export class HomeComponent implements OnInit {
+  registerMode = false;
+  values: any;
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    console.log('hello');
     this.getValues();
+  }
+  registerToggle() {
+    this.registerMode = true;
   }
   getValues() {
     this.http.get('http://localhost:5000/api/values').subscribe( response => {
@@ -21,5 +25,7 @@ values: any;
       console.log(error);
     });
   }
-
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
+  }
 }
---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  model: any = {};
+  @Input() valuesFromHome: any;
+  @Output() cancelRegister = new EventEmitter();
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('registered Successfully');
+    }, err => {
+      console.log(err);
+    });
+   
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('cancelled');
+  }
+
+
+}
